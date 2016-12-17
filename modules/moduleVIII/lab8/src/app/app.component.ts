a9a9a657f8ec6c58748af5f7860467c7b0880aa0
Edison Rosero <edisonroserosc@hotmail.com>
2016-12-17 12:13:04 -0500

    2016/12/17

--- modules/moduleVIII/lab8/src/app/app.component.ts
+++ modules/moduleVIII/lab8/src/app/app.component.ts
@@ -14,6 +14,14 @@ export class MyApp {
 
 constructor(platform: Platform, public userService: UserService) {
     platform.ready().then(() => {
+    var notificationOpenedCallback = function(jsonData) {
+    console.log('notificationOpenedCallback: ' + JSON.stringify(jsonData));
+  };
+  window["plugins"].OneSignal
+    .startInit("20729931-80ca-45af-b48a-a11d84589ff0", "574293351514")
+    .handleNotificationOpened(notificationOpenedCallback)
+    .endInit();
+
       StatusBar.styleDefault();
       Splashscreen.hide();
        this.userService.openDatabase()
